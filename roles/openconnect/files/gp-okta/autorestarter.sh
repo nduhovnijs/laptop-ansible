762f0ee28a6746de078e09eb6d1a12d29aa02bcc
Nikita Duhovnijs <nikita@duhovnijs.me>
2020-01-12 01:05:13 +0200

    Add autorestarter unfinished script for Openconnect (at least for history)

---
+++ roles/openconnect/files/gp-okta/autorestarter.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Never got to finish this
+
+cleanup ()
+{
+    echo "Interrupt caught, exiting"
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+pass okta | /opt/gp-okta/gp-okta.py /opt/gp-okta/gp-okta.conf
+
+while true; do
+    status=$(ps -ef | grep gp-okta | grep -v grep)
+    if [ -z "$status" ]; then
+        sleep 5
+        pass okta | /opt/gp-okta/gp-okta.py /opt/gp-okta/gp-okta.conf
+    fi
+    sleep 1
+done
+
+### -------
+# /etc/acpi/lid.sh 
+#!/bin/bash
+
+#grep -q open /proc/acpi/button/lid/*/state
+#if [ $? = 0 ]; then
+#    sleep 30
+#    kill $(ps -ef | grep gp-okta | grep -v grep | awk -F " " '{print $2}')
+#fi
+
+### --------
+# Put in file /etc/acpi/events/lm_lid
+# event=button/lid.*
+# action=/etc/acpi/lid.sh
+
+### --------
+# Restart /etc/init.d/acpid
